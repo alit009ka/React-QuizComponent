f979a12e689f66dcfc391c5a5d74dcdccd08125b
Tanveer Ali <taali@netcompany.com>
2022-11-01 10:43:01 +0100

    hi

---
+++ src/QuizQuestionButton.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+class QuizQuestionButton extends Component{
+    render(){
+        return (
+            <li>
+                <button onClick={ button_text }>
+                    
+                </button>
+            </li>
+        )
+    }
+}
+export default QuizQuestionButton
