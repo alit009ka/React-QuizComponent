891b8859e667680b535194cab0fcab412ffb1075
Tanveer Ali <taali@netcompany.com>
2022-11-01 13:49:38 +0100

    hi

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -8,10 +8,10 @@ class QuizQuestion extends Component {
   }
   handleClick(buttonText) {
     if(buttonText===this.props.quiz_question.answer){
-      this.setState({incoorectAnswer: false})
+      this.setState({incorrectAnswer: false})
       this.props.showNextQuestionHandler()
     }else{
-      this.setState({incoorectAnswer: true})
+      this.setState({incorrectAnswer: true })
     }
 
   }
